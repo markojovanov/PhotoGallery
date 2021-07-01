59306c32ec9d4565318d0b0f4b96e5d66c8180b9
markojovanov <markojovanov999@gmail.com>
2021-07-01 14:15:49 +0200

    Update v2.0

--- Project1/Project1/DetailViewController.swift
+++ Project1/Project1/DetailViewController.swift
@@ -1,31 +1,27 @@
-//
-//  DetailViewController.swift
-//  Project1
-//
-//  Created by Finki User on 30.6.21.
-//
-
 import UIKit
 
 class DetailViewController: UIViewController {
 
     @IBOutlet var Picture: UIImageView!
     var imageOutput: String?
+    var numberOfImage: Int?
+    var numberOfAllImages: Int?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        title=imageOutput
+        title="Picture \(numberOfImage!) of \(numberOfAllImages!)"
         navigationItem.largeTitleDisplayMode = .never
+        
         if let imageLoad = imageOutput {
             Picture.image = UIImage(named: imageLoad)
         }
-
-        // Do any additional setup after loading the view.
     }
+    
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         navigationController?.hidesBarsOnTap = true
     }
+    
     override func viewDidDisappear(_ animated: Bool) {
         super.viewDidDisappear(animated)
         navigationController?.hidesBarsOnTap = false
--- Project1/Project1/ViewController.swift
+++ Project1/Project1/ViewController.swift
@@ -1,13 +1,7 @@
-//
-//  ViewController.swift
-//  Project1
-//
-//  Created by Finki User on 30.6.21.
-//
-
 import UIKit
 
 class ViewController: UITableViewController {
+    
     var pictures = [String]()
     
     override func viewDidLoad() {
@@ -23,22 +17,31 @@ class ViewController: UITableViewController {
             }
         }
     }
+    
+    func sortPictures(pictures: [String]) -> [String]{
+        return pictures.sorted();
+    }
+    
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         navigationController?.hidesBarsOnTap = false
     }
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return pictures.count
     }
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
-        cell.textLabel?.text = pictures[indexPath.row]
+        cell.textLabel?.text = sortPictures(pictures: pictures)[indexPath.row]
         return cell
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if let vc = storyboard?.instantiateViewController(identifier: "Detail") as? DetailViewController {
-            vc.imageOutput=pictures[indexPath.row]
+            vc.imageOutput=sortPictures(pictures: pictures)[indexPath.row]
+            vc.numberOfImage=indexPath.row + 1
+            vc.numberOfAllImages=pictures.count
             navigationController?.pushViewController(vc, animated: true)
         }
     }
